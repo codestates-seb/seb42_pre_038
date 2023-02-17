9f29b25234e060fd27107aa9a708f315b2c81826
heyroocoke <heyroocoke@gmail.com>
2023-02-17 14:12:30 +0900

    docs: 페이지분기 및 컴포넌트 생성

---
+++ client/src/components/LeftNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftNavBar = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default LeftNavBar;
---
+++ client/src/pages/AnswerEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnswerEdit = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default AnswerEdit;
