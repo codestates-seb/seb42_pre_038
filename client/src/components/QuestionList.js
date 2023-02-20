7f847b60f9bd6a109cd0ab4dc2e3ddc34bc1ee23
18137036 <9wjdgh9@naver.com>
2023-02-20 16:20:34 +0900

    design :  QuestionsList HeadBox font-weight 수정

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -14,6 +14,7 @@ const QuestionListHeadBox = styled.div`
 const QuestionListTitle = styled.h1`
   flex: 1;
   font-size: 2.07692308rem;
+  font-weight: normal;
 `;
 
 const AskButton = styled.button`
