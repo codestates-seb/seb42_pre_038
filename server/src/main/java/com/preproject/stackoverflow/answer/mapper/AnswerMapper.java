4d99d859b78e470ff3d4122231f00d0f42060a7b
NakChoi <nakchoi94@gmail.com>
2023-02-20 10:56:29 +0900

    feat: AnswerMapper 추가

---
+++ server/src/main/java/com/preproject/stackoverflow/answer/mapper/AnswerMapper.java
@@ -0,0 +1,21 @@
+package com.preproject.stackoverflow.answer.mapper;
+
+
+import com.preproject.stackoverflow.answer.dto.AnswerPatchDto;
+import com.preproject.stackoverflow.answer.dto.AnswerPostDto;
+import com.preproject.stackoverflow.answer.dto.AnswerResponseDto;
+import com.preproject.stackoverflow.answer.entity.Answer;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface AnswerMapper {
+
+    Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto);
+    Answer answerPatchDtoToAnswer(AnswerPatchDto answerPatchDto);
+
+    AnswerResponseDto answerToAnswerResponseDto(Answer answer);
+
+
+}
