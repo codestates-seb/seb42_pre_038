c0c809c75c568fb192e3b309beb0581cee454838
chanheek <toadfwe1@naver.com>
2023-02-20 17:07:28 +0900

    design: Header 수정

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,41 +3,46 @@ import Logo from '../img/Logo.png';
 import SearchIcon from '@mui/icons-material/Search';
 // const isLogin = true;
 const HeaderWrap = styled.div`
+  align-items: center;
+  box-sizing: border-box;
+  background-color: rgb(248, 249, 249);
+  border-top: 3px solid rgb(244, 130, 36);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), 0 1px 4px rgba(0, 0, 0, 0.05),
+    0 2px 8px rgba(0, 0, 0, 0.05);
   display: flex;
+  height: 50px;
+  left: 0;
   position: sticky;
-  top: 0px;
-  z-index: 10000;
-  min-height: 5vh;
   width: 100vw;
-  height: 47.6px;
+  z-index: 100;
 `;
 const HeaderContainer = styled.div`
+  min-height: 5vh;
+  width: 1264px;
+  height: 47.6px;
+  margin: 0 127.6px;
   display: flex;
-  flex-direction: row;
-  width: 100%;
   align-items: center;
-  justify-content: space-around;
-  background-color: #fbfafa;
-  box-shadow: 0px 0.5px 2px rgba(0, 0, 5);
-  border-top: 2px solid red;
+  justify-content: center;
 `;
 const HeaderLeft = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-top: 0 10px;
+  margin: 10px;
   > img {
+    box-sizing: content-box;
     width: 150px;
-    object-fit: contain;
-    padding: 0;
+    height: 30px;
+    padding: 0 8px;
     cursor: pointer;
   }
   > p {
     font-weight: 100;
-    font-size: 14px;
-    margin: 0px 10px;
+    font-size: 12px;
+    padding: 0 2px;
+    box-sizing: content-box;
     cursor: pointer;
-    padding: 5px 10px;
     :hover {
       background-color: #ddd;
       border-radius: 33px;
@@ -48,51 +53,67 @@ const HeaderMiddle = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding: 10px 10px;
+  padding: 0;
 `;
 const HeaderSearchContainer = styled.div`
   display: flex;
-  margin-right: 10px;
   align-items: center;
-  background-color: #fff;
-  border-radius: 3px;
-  border: 1px solid black;
+  padding: 0;
+  margin-right: 10px;
   > input {
+    padding: 0 10px;
     border: none;
     margin-left: 5px;
     outline: none;
-    width: 100%;
+    width: 800px;
+    border-radius: 3px;
   }
   > .MuiSvgIcon-root {
-    color: #ccc;
+    color: gray;
+    width: 25px;
+    height: 25px;
   }
 `;
 const HeaderRight = styled.div`
   display: flex;
   align-items: center;
-  padding: 5px 10px;
+  width: 212.913px;
   cursor: pointer;
-  .HeaderLogoutBox {
+  .Login {
+    background-color: #e1ecf4;
     display: flex;
     padding: 5px 10px;
-    margin: 0 10px;
-    background-color: red;
-    width: 100px;
+    margin-right: 5px;
+    margin-left: 5px;
+    background-color: #e1ecf4;
+    width: 50px;
     justify-content: center;
     align-items: center;
-    border-radius: 10px;
-    font-size: 15px;
-    font-weight: 700;
+    border-radius: 3px;
+    font-size: 12px;
+    border: 1px solid #0a95ff;
+  }
+  .Signup {
+    color: white;
+    font-weight: 600;
+    display: flex;
+    padding: 5px 10px;
+    background-color: #0a95ff;
+    width: 50px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 3px;
+    font-size: 12px;
   }
   .HeaderUsersBox {
     color: white;
     background-color: gray;
-    width: 30px;
-    height: 30px;
+    width: 24px;
+    height: 24px;
     align-items: center;
     justify-content: center;
     display: flex;
-    border-radius: 10px;
+    border-radius: 3px;
     font-size: 10px;
     font-weight: 800;
   }
@@ -114,7 +135,8 @@ const Header = () => {
         </HeaderMiddle>
         <HeaderRight>
           <div className="HeaderUsersBox">찬희</div>
-          <div className="HeaderLogoutBox">Logout</div>
+          <div className="Login">Login</div>
+          <div className="Signup">Singup</div>
         </HeaderRight>
       </HeaderContainer>
     </HeaderWrap>
