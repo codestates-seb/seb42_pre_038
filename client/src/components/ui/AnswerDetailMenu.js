2bd303562d2885541059498ad769ddf35ad7537f
18137036 <9wjdgh9@naver.com>
2023-02-21 15:38:04 +0900

    design : common component AnswerDetailMenu 구현

---
+++ client/src/components/ui/AnswerDetailMenu.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import User from '../../images/avatar.png';
+const QuestionDetialMenuBox = styled.div`
+  display: flex;
+  margin-top: 24px;
+`;
+const MenuBox = styled.div`
+  flex: 1;
+`;
+const MenuSpan = styled.span`
+  cursor: pointer;
+  margin: calc(var(--su8) / 2);
+  color: var(--_an-a-fc, inherit);
+  &:hover {
+    color: hsl(210deg 8% 55%);
+  }
+`;
+const UserInfoBox = styled.div`
+  width: 200px;
+  border-radius: 3px;
+  padding: 5px 6px 7px 7px;
+`;
+const UserSpanBox = styled.div`
+  margin-top: 1px;
+  margin-bottom: 4px;
+`;
+
+const UserSpan = styled.span`
+  font-size: 12px;
+  color: hsl(210deg 8% 45%);
+`;
+const UserDetailImgBox = styled.div`
+  float: left;
+  width: 32px;
+  height: 32px;
+`;
+const UserImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 3px;
+`;
+
+const UserDetailBox = styled.div`
+  float: left;
+  margin-left: 8px;
+  width: calc(100% - 40px);
+`;
+
+const UserDetailSpan = styled.span`
+  color: hsl(206deg 100% 40%);
+  font-size: 13px;
+`;
+
+const AnswerDetailMenu = () => {
+  return (
+    <QuestionDetialMenuBox>
+      <MenuBox>
+        <MenuSpan>Share</MenuSpan>
+        <MenuSpan>Edit</MenuSpan>
+        <MenuSpan>Follow</MenuSpan>
+      </MenuBox>
+      <UserInfoBox>
+        <UserSpanBox>
+          <UserSpan>answered 9 mins ago</UserSpan>
+        </UserSpanBox>
+        <UserDetailImgBox>
+          <UserImg src={User}></UserImg>
+        </UserDetailImgBox>
+        <UserDetailBox>
+          <UserDetailSpan>Grzegorz Adam Hankiewicz</UserDetailSpan>
+        </UserDetailBox>
+      </UserInfoBox>
+    </QuestionDetialMenuBox>
+  );
+};
+
+export default AnswerDetailMenu;
