8e94c1249032a875f187a2497603c758ded23840
heyroocoke <heyroocoke@gmail.com>
2023-02-21 10:09:45 +0900

    feat: Userinfo 생성

--- client/src/pages/UserInfo.js
+++ client/src/pages/UserInfo.js
@@ -1,5 +1,22 @@
+import UserInfoHeader from '../components/UserInfoHeader';
+import LeftNavBar from '../components/main/LeftNavBar';
+import styled from 'styled-components';
+import { MainBox } from './Home';
+
+const UserInfoWrap = styled.div`
+  display: flex;
+`;
 const UserInfo = () => {
-  return <div></div>;
+  return (
+    <>
+      <LeftNavBar />
+      <MainBox>
+        <UserInfoWrap>
+          <UserInfoHeader />
+        </UserInfoWrap>
+      </MainBox>
+    </>
+  );
 };
 
 export default UserInfo;
