a4e4275d66ba321dbaadf68ba99c0e304b9ec56f
heyroocoke <heyroocoke@gmail.com>
2023-02-21 10:08:59 +0900

    docs: 문서구조 변경

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,27 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/main/Footer';
 import Header from './components/main/Header';
-import Home from './pages/Home';
+import Home, { ContainerBox } from './pages/Home';
 import Login from './pages/Login';
 import QuestionCreate from './pages/QuestionCreate';
 import Signup from './pages/Signup';
 import QuestionDetail from './pages/QuestionDetail';
+import UserInfo from './pages/UserInfo';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/questions/ask" element={<QuestionCreate />} />
-        <Route path="/questions/:id" element={<QuestionDetail />} />
-      </Routes>
+      <ContainerBox>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/questions/ask" element={<QuestionCreate />} />
+          <Route path="/questions/:id" element={<QuestionDetail />} />
+          <Route path="/userinfo" element={<UserInfo />} />
+        </Routes>
+      </ContainerBox>
       <Footer />
     </BrowserRouter>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,14 @@ import LeftNavBar from '../components/main/LeftNavBar';
 import QuestionList from '../components/question/QuestionList';
 import styled from 'styled-components';
 
-const ContainerBox = styled.div`
+export const ContainerBox = styled.div`
   max-width: 1264px;
   display: flex;
   justify-content: space-between;
   margin: 0 auto;
 `;
 
-const MainBox = styled.section`
+export const MainBox = styled.section`
   max-width: 1100px;
   margin: 0 auto;
   display: flex;
@@ -19,13 +19,11 @@ const MainBox = styled.section`
 const Home = () => {
   return (
     <>
-      <ContainerBox>
-        <LeftNavBar />
-        <MainBox>
-          <QuestionList />
-          <RightSideBar />
-        </MainBox>
-      </ContainerBox>
+      <LeftNavBar />
+      <MainBox>
+        <QuestionList />
+        <RightSideBar />
+      </MainBox>
     </>
   );
 };
