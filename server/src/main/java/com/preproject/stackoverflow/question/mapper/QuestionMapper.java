2f2491a4cb34ce90fa95b9b25641f95fcd970a8a
ciaomin <slswk1997@gmail.com>
2023-02-24 17:11:12 +0900

    feat: QuestionMapper 변경

--- server/src/main/java/com/preproject/stackoverflow/question/mapper/QuestionMapper.java
+++ server/src/main/java/com/preproject/stackoverflow/question/mapper/QuestionMapper.java
@@ -3,7 +3,6 @@ package com.preproject.stackoverflow.question.mapper;
 import com.preproject.stackoverflow.question.dto.QuestionPostDto;
 import com.preproject.stackoverflow.question.dto.QuestionPatchDto;
 import com.preproject.stackoverflow.question.dto.QuestionResponseDto;
-import com.preproject.stackoverflow.question.entity.Question;
 import org.mapstruct.Mapper;
 
 import java.util.List;
