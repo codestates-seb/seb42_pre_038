9f323b13062cb4efa02304fef45ce2fbb62490f8
18137036 <9wjdgh9@naver.com>
2023-02-24 21:48:02 +0900

    design : Quill Editor 사용

--- client/src/components/answer/AnswerCreate.js
+++ client/src/components/answer/AnswerCreate.js
@@ -11,21 +11,42 @@ const AnswerCountSpan = styled.h2`
   padding-top: 20px;
 `;
 
-const AnswerInputBox = styled.div`
-  height: 250px;
-`;
-
 const AnswerButtonBox = styled.div`
   padding: 20px 0 15px 0;
 `;
 const AnswerCreate = () => {
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ font: [] }],
+        [{ align: [] }],
+        ['image'],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+        [
+          {
+            color: [
+              '#000000',
+              '#e60000',
+              '#ff9900',
+              '#ffff00',
+              '#008a00',
+              '#0066cc',
+              '#9933ff',
+              'custom-color',
+            ],
+          },
+          { background: [] },
+        ],
+      ],
+    },
+  };
+
   return (
     <AnswerCreateWrap>
       <AnswerCountSpan>Your Answer</AnswerCountSpan>
-      <AnswerInputBox>
-        <ReactQuill />
-        {/* <AnsweTextArea></AnsweTextArea> */}
-      </AnswerInputBox>
+      <ReactQuill modules={modules} />
       <AnswerButtonBox>
         <MainButton>Post Your Answer</MainButton>
       </AnswerButtonBox>
