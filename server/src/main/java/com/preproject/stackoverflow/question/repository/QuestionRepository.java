0e5f48bc30944734c027f41cee2860e222bdfcee
ciaomin <slswk1997@gmail.com>
2023-02-24 16:59:56 +0900

    QuestionRepository 변경

--- server/src/main/java/com/preproject/stackoverflow/question/repository/QuestionRepository.java
+++ server/src/main/java/com/preproject/stackoverflow/question/repository/QuestionRepository.java
@@ -1,9 +1,16 @@
 package com.preproject.stackoverflow.question.repository;
 
+import com.preproject.stackoverflow.answer.entity.Answer;
 import com.preproject.stackoverflow.question.entity.Question;
+import com.preproject.stackoverflow.question.entity.QuestionVote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Long countQuestionByAnswer_AnswerId(Long answerId);
+    Page<Question> findByQuestion(Long questionId, Pageable pageable);;
 }
 
