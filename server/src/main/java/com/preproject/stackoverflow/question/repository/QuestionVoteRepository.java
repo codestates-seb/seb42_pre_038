92d6848d7019f827a88616ce0ecd5e4352836457
ciaomin <slswk1997@gmail.com>
2023-02-24 17:12:14 +0900

    QuestionVoteRepository 추가

--- server/src/main/java/com/preproject/stackoverflow/question/repository/QuestionVoteRepository.java
+++ server/src/main/java/com/preproject/stackoverflow/question/repository/QuestionVoteRepository.java
@@ -1,7 +1,6 @@
 package com.preproject.stackoverflow.question.repository;
 
 
-import com.preproject.stackoverflow.question.entity.QuestionVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
