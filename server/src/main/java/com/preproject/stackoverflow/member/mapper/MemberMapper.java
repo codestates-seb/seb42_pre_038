11fbed95d858814d038200fc627b9d7491244725
ichubtou <msjj0809@naver.com>
2023-02-27 16:59:00 +0900

    refactor: MemberMapper 기능 수정

--- server/src/main/java/com/preproject/stackoverflow/member/mapper/MemberMapper.java
+++ server/src/main/java/com/preproject/stackoverflow/member/mapper/MemberMapper.java
@@ -2,8 +2,12 @@ package com.preproject.stackoverflow.member.mapper;
 
 import com.preproject.stackoverflow.member.dto.MemberDto;
 import com.preproject.stackoverflow.member.entity.Member;
+import com.preproject.stackoverflow.question.dto.QuestionResponseDto;
+import com.preproject.stackoverflow.question.entity.Question;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
