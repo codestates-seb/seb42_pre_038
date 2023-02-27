1bfa6021f072a9387ad154d78a4e58b4315ea7e6
ichubtou <msjj0809@naver.com>
2023-02-27 17:07:13 +0900

    refactor: QuestionResponseDto 기능 수정

--- server/src/main/java/com/preproject/stackoverflow/question/dto/QuestionResponseDto.java
+++ server/src/main/java/com/preproject/stackoverflow/question/dto/QuestionResponseDto.java
@@ -1,12 +1,13 @@
 package com.preproject.stackoverflow.question.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Builder // 여러 패턴의 생성자 생성
+@AllArgsConstructor
 @Getter
 @Setter
 public class QuestionResponseDto {
@@ -17,11 +18,10 @@ public class QuestionResponseDto {
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
     private int voteCount; // vote
-    private Long answerId;
     private Long memberId;
-    private String name;
-    private String viewCount;
+    private int viewCount;
     private int answersCount;
+    private String name;
 
 
 }
