ba38234f6552ccee588a8ed6d3b18c8d2be3c8b6
ichubtou <msjj0809@naver.com>
2023-02-27 17:08:40 +0900

    refactor: QuestionMapper 수정

--- server/src/main/java/com/preproject/stackoverflow/question/mapper/QuestionMapper.java
+++ server/src/main/java/com/preproject/stackoverflow/question/mapper/QuestionMapper.java
@@ -1,19 +1,29 @@
 package com.preproject.stackoverflow.question.mapper;
 
+import com.preproject.stackoverflow.answer.entity.Answer;
+import com.preproject.stackoverflow.member.entity.Member;
 import com.preproject.stackoverflow.question.dto.QuestionPostDto;
 import com.preproject.stackoverflow.question.dto.QuestionPatchDto;
 import com.preproject.stackoverflow.question.dto.QuestionResponseDto;
 import com.preproject.stackoverflow.question.entity.Question;
+import com.preproject.stackoverflow.question.entity.QuestionVote;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 
 public interface QuestionMapper {
 
+    @Mapping(source = "memberId", target = "member.memberId")
+    @Mapping(source = "name", target = "member.name")
     Question questionPostDtoToQuestion(QuestionPostDto questionPostDto);
     Question questionPatchDtoToQuestion(QuestionPatchDto questionPatchDto);
+
+    @Mapping(source = "member.memberId", target = "memberId")
+    @Mapping(source = "member.name", target = "name")
     QuestionResponseDto questionToQuestionResponseDto(Question question);
 
     List<QuestionResponseDto> questionsToQuestionResponseDto(List<Question> questions);
