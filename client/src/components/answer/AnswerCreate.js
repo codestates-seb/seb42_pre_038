5e31ddba5b684b7af0474eae44ebee896fe083d8
18137036 <9wjdgh9@naver.com>
2023-02-28 03:15:22 +0900

    feat : creat answer post 요청

--- client/src/components/answer/AnswerCreate.js
+++ client/src/components/answer/AnswerCreate.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import MainButton from '../ui/MainButton';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { postCreateAnswer } from '../../api/answerAPI';
 
 const AnswerCreateWrap = styled.div``;
 const AnswerCountSpan = styled.h2`
@@ -15,8 +18,25 @@ const AnswerButtonBox = styled.div`
   padding: 20px 0 15px 0;
 `;
 const AnswerCreate = () => {
-  const goYouAnswer = () => {
+  const [answerValue, setAnswerValue] = useState('');
+  const { id } = useParams();
+
+  const handleAnswerChange = (e) => {
+    setAnswerValue(e);
+    console.log(e);
+  };
+  const handlerClickAnswer = () => {
     console.log('post answer');
+    console.log(answerValue);
+    postCreatAnswerFun();
+  };
+
+  const postCreatAnswerFun = async () => {
+    const res = await postCreateAnswer(id, answerValue);
+
+    if (res) {
+      window.location.reload();
+    }
   };
   const modules = {
     toolbar: {
@@ -24,7 +44,6 @@ const AnswerCreate = () => {
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
         [{ font: [] }],
         [{ align: [] }],
-        ['image'],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
         [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
         [
@@ -49,9 +68,15 @@ const AnswerCreate = () => {
   return (
     <AnswerCreateWrap>
       <AnswerCountSpan>Your Answer</AnswerCountSpan>
-      <ReactQuill modules={modules} />
+      <ReactQuill
+        modules={modules}
+        value={answerValue}
+        onChange={handleAnswerChange}
+      />
       <AnswerButtonBox>
-        <MainButton ButtonProps={goYouAnswer}>Post Your Answer</MainButton>
+        <MainButton ButtonProps={handlerClickAnswer}>
+          Post Your Answer
+        </MainButton>
       </AnswerButtonBox>
     </AnswerCreateWrap>
   );
