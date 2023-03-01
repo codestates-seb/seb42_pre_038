3de1da76568776437f3f7993ade7c0d1f153cf83
heyroocoke <heyroocoke@gmail.com>
2023-03-01 22:43:52 +0900

    feat: UserProfile 페이지 구현

--- client/src/pages/UserInfo.js
+++ client/src/pages/UserInfo.js
@@ -2,12 +2,13 @@ import UserInfoHeader from '../components/userinfo/UserInfoHeader';
 import LeftNavBar from '../components/main/LeftNavBar';
 import styled from 'styled-components';
 import { ContainerBox } from './Home';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import UserInfoLeftNav from '../components/userinfo/UserInfoLeftNav';
 import UserInfoEditProfile from '../components/userinfo/UserInfoEditProfile';
 import PropTypes from 'prop-types';
-import UserProfileItem from '../components/userinfo/UserProfileItem';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import UserProfileList from '../components/userinfo/UserProfileList';
+import axios from 'axios';
 
 const UserInfoWrap = styled.div`
   max-width: 1100px;
@@ -43,6 +44,50 @@ const UserInfo = () => {
   const [userHeaderTap, setUserHeaderTap] = useState('userinfo');
   const [EditDelete, setEditDelete] = useState('EditProfile');
 
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  // eslint-disable-next-line no-undef
+  const URI = process.env.REACT_APP_SERVER_URI;
+  const memberId = localStorage.getItem('memberId');
+  const header = {
+    headers: {
+      Authorization: token,
+    },
+  };
+
+  const [isUserContents, setIsUserContents] = useState([]);
+  const [isUserAnswers, setIsUserAnswers] = useState([]);
+  useEffect(() => {
+    // 유저 프로필 퀘스천 요청
+    axios
+      .get(`${URI}/api/questions/profile/${memberId}`, header)
+      .then((res) => {
+        console.log(res);
+        setIsUserContents(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(error.response.status);
+        if (error.response.status === 401) {
+          localStorage.clear();
+          navigate('/login');
+        }
+      });
+
+    // 유저 프로필 앤써 요청
+    axios
+      .get(`${URI}/api/answers/profile/${memberId}`, header)
+      .then((res) => setIsUserAnswers(res.data.data))
+      .catch((error) => {
+        console.log(error);
+        console.log(error.response.status);
+        if (error.response.status === 401) {
+          localStorage.clear();
+          navigate('/login');
+        }
+      });
+  }, []);
+
   const { id } = useParams();
   console.log(id);
   return (
@@ -75,12 +120,10 @@ const UserInfo = () => {
               <div className="d-grid g24">
                 <div data-can-be="stats" data-is-here-when="sm md"></div>
                 <div className="grid--item">
-                  <div className="fs-title mb8">Qeustions</div>
-                  <div className="s-empty-state p32 bg-black-025 bar-md ba bc-black-075 ta-center">
+                  <div className="fs-title mb8">Questions</div>
+                  <div className="s-empty-state p24 bar-md ba bc-black-075 ta-center">
                     <p className="mx-auto wmx3 mb0"></p>
-                    <UserProfileItem />
-                    <UserProfileItem />
-                    <UserProfileItem />
+                    <UserProfileList isUserContents={isUserContents} />
                   </div>
                 </div>
 
@@ -88,13 +131,9 @@ const UserInfo = () => {
                   <div className="d-flex ai-center jc-space-between mb8">
                     <div className="flex--item fs-title">Answers</div>
                   </div>
-                  <div className="s-empty-state p32 bg-black-025 bar-md ba bc-black-075 ta-center">
+                  <div className="s-empty-state p24 bar-md ba bc-black-075 ta-center">
                     <p className="mx-auto wmx3 mb0"></p>
-                    <ul>
-                      <li>1</li>
-                      <li>2</li>
-                      <li>3</li>
-                    </ul>
+                    <UserProfileList isUserAnswers={isUserAnswers} />
                   </div>
                 </div>
               </div>
